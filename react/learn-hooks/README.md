a2608cd6719d452f2703c962130aa05f094b9217
fuu <unifuusawatari@gmail.com>
2022-09-08 06:16:02 +0900

    darkmode failed

--- react/learn-hooks/README.md
+++ react/learn-hooks/README.md
@@ -1,5 +1,9 @@
 # Learn Hooks
 
+- [ ] Learn the use of Hooks
+- [ ] Learn to make a dark theme
+
 ## Reference
 
 - [React Tutorial - W3Schools](https://www.w3schools.com/REACT/react_hooks.asp)
+- [How to Create a Dark Mode Component in React](https://dev.to/alexeagleson/how-to-create-a-dark-mode-component-in-react-3ibg)
--- react/learn-hooks/src/App.tsx
+++ react/learn-hooks/src/App.tsx
@@ -2,21 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import { Link } from "react-router-dom";
 import './App.css';
+import DarkMode from './DarkMode';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <Link to="/useState">UseState</Link> |{" "}
-        <Link to="/useEffect">UseEffect</Link> |{" "}
-        <Link to="/useContext">UseContext</Link>
-      </nav>
+        <DarkMode />
+        <nav
+          style={{
+            borderBottom: "solid 1px",
+            paddingBottom: "1rem",
+          }}
+        >
+          <Link to="/useState">UseState</Link> |{" "}
+          <Link to="/useEffect">UseEffect</Link> |{" "}
+          <Link to="/useContext">UseContext</Link>
+        </nav>
+
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
